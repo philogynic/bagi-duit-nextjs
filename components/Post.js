83ad7734a79cda8103baba1aa781ece28553fb48
philogynic <bimogame161@gmail.com>
2021-01-07 21:15:10 +0700

    adding initial posts page

---
+++ components/Post.js
@@ -0,0 +1,15 @@
+import {Box} from '@chakra-ui/react'
+
+export default function Post() {
+    return (
+        <Box m={2}>
+            <Box backgroundColor='gray.100' maxW='200px'>
+                2k dari Jono
+            </Box>
+            <Box backgroundColor='gray.200' p={2}>
+                will sdsd be a post hahahahahahahahahahahah nih duit nih buat loooo
+            </Box>
+        </Box>
+        
+    )
+}
---
+++ pages/posts.js
@@ -0,0 +1,37 @@
+
+import {Flex, Spacer, Box} from '@chakra-ui/react'
+import Post from '../components/Post'
+
+
+export default function PostList() {
+  return (
+    <Flex justifyContent='center' >
+        <Box mt={8} maxW='400px'>
+
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+          <Post/>
+
+        </Box>
+    </Flex>
+    
+  )
+}
